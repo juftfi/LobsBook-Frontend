57b692e147688eb07af6cc3a6f1acd26e320b0df
juftfi <107974770+juftfi@users.noreply.github.com>
2026-08-03 21:40:49 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="hidden md:flex items-center gap-4 text-sm">
-          <Link to="https://www.flap.sh" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
+          <Link to="https://flap.sh/bnb/0x64491b5f87653292bbed0f7ce6d69351cc707777" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
             $LOBS
           </Link>
           <Link to="/communities" className="text-white hover:text-white/80 transition-colors font-bold">
